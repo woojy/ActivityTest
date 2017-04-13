7987253a9dbee5b605f6f9b233c0d71c02494282
appcreate35 <appcreate35@appcreate35ui-iMac.local>
2017-04-13 20:22:17 +0900

    #3 finish

--- app/src/main/java/woojy/kr/hs/emirim/activitytest/MainActivity.java
+++ app/src/main/java/woojy/kr/hs/emirim/activitytest/MainActivity.java
@@ -1,7 +1,11 @@
 package woojy.kr.hs.emirim.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ScrollingTabContainerView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button butMain=(Button)findViewById(R.id.but_open);
+        butMain.setOnClickListener(butMainHandler);
     }
+
+    View.OnClickListener butMainHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(intent);
+        }
+    };
 }
--- app/src/main/java/woojy/kr/hs/emirim/activitytest/SecondActivity.java
+++ app/src/main/java/woojy/kr/hs/emirim/activitytest/SecondActivity.java
@@ -2,6 +2,8 @@ package woojy.kr.hs.emirim.activitytest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        Button butBack = (Button)findViewById(R.id.but_second);
+        butBack.setOnClickListener(butBackHandler);
     }
+    View.OnClickListener butBackHandler = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            finish();
+        }
+    };
 }
